68c4acbfabba60082c0398edba728de0163e3264
David Zambrano <david.zambrano@duwest.com>
2018-03-02 09:07:39 -0500

    Mensaje batch

---
+++ mensajeGitHub.bat
@@ -0,0 +1,11 @@
+@echo off
+color 2F
+Title MENSAJE DESDE GITHUB - DAVID ZAMBRANO
+echo =================================================================
+echo Saludos profesor este es el mensaje solicitado enviar desde GItHUb
+echo Cordial Saludo
+echo David Zambrano
+echo ===================================================================
+timeout /t 10
+rundll32.exe user32.dll
+exit
